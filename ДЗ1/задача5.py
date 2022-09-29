6bb971fd6d4671db3beaa211ab6ab689ed3edde3
MuuurK25 <markderkachevazaza@mail.ru>
2022-09-29 21:39:21 +0300

    Изменил решение задачи номер 5

--- ДЗ1/задача5.py
+++ ДЗ1/задача5.py
@@ -3,9 +3,11 @@
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
 
+from math import sqrt
+
 xA = float(input('Введите координату точки А по оси Х: '))
 yA = float(input('Введите координату точки А по оси Y: '))
 xB = float(input('Введите координату точки B по оси Х: '))
 yB = float(input('Введите координату точки B по оси Y: '))
-distance = round(((xB - xA)**2 + (yB - yA)**2)**0.5, 2)
+distance = round(sqrt((xB - xA)**2 + (yB - yA)**2), 2)
 print(distance)
